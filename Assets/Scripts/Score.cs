b3a100ccac41055074af8613a1d97bd6f75a9b14
HansaeJu <sparky@hanyang.ac.kr>
2020-11-10 20:05:24 +0900

    Score ADDED

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text countText;
+    private int travel;
+    public Transform player;
+
+    public int biasY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        travel = (int)(-player.position.y * 2) + biasY;
+        countText.text = travel.ToString() + "m";
+    }
+}
