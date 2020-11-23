71cfcfbfdac2e79065201bc6b86af440e58e2787
주한새 <tiger2714@naver.com>
2020-11-23 15:38:07 +0900

    Display Ratio Issue Resolved

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -14,22 +14,9 @@ public class CameraController : MonoBehaviour
     private void Awake()
     {
         Camera camera = GetComponent<Camera>();
-        Rect rect = camera.rect;
-
-        float scaleHeight = ((float)Screen.width / Screen.height) / ((float)9 / 16);
-        float scaleWidth = 1f / scaleHeight;
-
-        if (scaleHeight < 1)
-        {
-            rect.height = scaleHeight;
-            rect.y = (1f - scaleHeight) / 2f;
-        }
-        else
-        {
-            rect.width = scaleWidth;
-            rect.x = (1f - scaleWidth) / 2f;
-        }
-        camera.rect = rect;
+        float ratio = Screen.height / (Screen.width / 9f);
+        float cameraSize = ratio * 5f / 16f;
+        camera.orthographicSize = cameraSize;
     }
 
     private void LateUpdate()
